9644697270040734b777c43ccb054d04e3dc735f
Antti Varis <ajvari88@gmail.com>
2025-01-23 21:45:49 +0200

    Viikko 2 - pieniä korjauksia

--- Viikko_2_ModelAndView/Viikkotehtävät/4-FriendListForm/src/main/java/chapter2/friend_form_result/web/FriendController.java
+++ Viikko_2_ModelAndView/Viikkotehtävät/4-FriendListForm/src/main/java/chapter2/friend_form_result/web/FriendController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import chapter2.friend_form_result.model.Friend;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 
 @Controller
@@ -23,27 +21,12 @@ public class FriendController {
     friends.add(new Friend("Jukka", "Jukkanen"));
   }
 
-  @RequestMapping(value="*", method=RequestMethod.GET)
-  public String requestMethodName() {
-      return "redirect:/friends";
-  }
-  
-
-
-
-
-
-
-
-
   @RequestMapping(value="/friends", method=RequestMethod.GET)
   public String requestMethodName(Model model) {
     model.addAttribute("friends", friends);
     return "friends";
   }
   
-  
-
   @RequestMapping(value="/add", method=RequestMethod.GET)
   public String showFriendForm(Model model) {
     model.addAttribute("friend", new Friend());
@@ -53,6 +36,11 @@ public class FriendController {
   @RequestMapping(value="/newfriend", method=RequestMethod.POST)
   public String addNewFriend(Friend friend) {
     FriendController.friends.add(friend);
+    return "redirect:/friends";
+  }
+
+  @RequestMapping(value="*", method=RequestMethod.GET)
+  public String requestMethodName() {
     return "redirect:/friends";
   }
   
