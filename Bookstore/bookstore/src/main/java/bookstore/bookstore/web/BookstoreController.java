a92ec0c25eb0bb60f04aff5182821aab1eff8565
Antti Varis <ajvari88@gmail.com>
2025-01-30 23:34:54 +0200

    Chapter 3 teh 4 & 5

--- Bookstore/bookstore/src/main/java/bookstore/bookstore/web/BookstoreController.java
+++ Bookstore/bookstore/src/main/java/bookstore/bookstore/web/BookstoreController.java
@@ -5,7 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import bookstore.bookstore.model.Book;
 import bookstore.bookstore.model.BookRepository;
@@ -35,18 +34,57 @@ public class BookstoreController {
   }
   
 
-  @RequestMapping(value="addbook", method=RequestMethod.POST)
-  public String saveBook(Book book) {
-    bookRepository.save(book);
+  // Used to save books from either creating a new book or editing an existing one
+  @RequestMapping(value={"/savebook","/savebook/{id}"}, method=RequestMethod.POST)
+  public String saveBook(Book book, @PathVariable(required = false) Long id) {
+
+    // ID not in request, so should be a new book. Save it to DB
+    if (id == null) {
+      System.out.println("Uusi kirja");
+      bookRepository.save(book);
+
+    // ID in request, so should be editing of existing book
+    } else {
+      System.out.println("Vanha kirja");
+
+      // Try to find existing book. If can't find it, redirect to booklist
+      Book oldBook = bookRepository.findById(id).orElse(null);
+      if (oldBook == null) {
+        return "redirect:/booklist";
+      }
+
+      // Book found. Update all values and save it
+      oldBook.setTitle(book.getTitle());
+      oldBook.setAuthor(book.getAuthor());
+      oldBook.setIsbn(book.getIsbn());
+      oldBook.setPublicationYear(book.getPublicationYear());
+      oldBook.setPrice(book.getPrice());
+      bookRepository.save(oldBook);
+    }
     return "redirect:/booklist";
   }
 
   
   @RequestMapping(value="/delete/{id}", method=RequestMethod.GET)
-  public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+  public String deleteBook(@PathVariable("id") Long bookId) {
     bookRepository.deleteById(bookId);
     return "redirect:../booklist";
   }
+
+
+  @RequestMapping(value="/edit/{id}", method=RequestMethod.GET)
+  public String editBook(@PathVariable("id") Long bookId, Model model) {
+    Book editBook = bookRepository.findById(bookId).orElse(null);
+    System.out.println(editBook);
+    
+    if (editBook == null) {
+      return "redirect:../booklist";
+    } else {
+      model.addAttribute("book", editBook);
+      return "editbook";
+    }
+  }
+  
 
 
   @RequestMapping(value="index", method=RequestMethod.GET)
