1b7cd5550656204c3765050a3534c5c26e1cc7ce
Antti Varis <ajvari88@gmail.com>
2025-01-30 21:43:16 +0200

    Chapter 3 teh 2

--- Bookstore/bookstore/src/main/java/bookstore/bookstore/web/BookstoreController.java
+++ Bookstore/bookstore/src/main/java/bookstore/bookstore/web/BookstoreController.java
@@ -1,22 +1,39 @@
 package bookstore.bookstore.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import bookstore.bookstore.model.BookRepository;
 
 
 @Controller
 public class BookstoreController {
 
+  private BookRepository bookRepository;
+
+  public BookstoreController(BookRepository bookRepository) {
+    this.bookRepository = bookRepository;
+  }
+
+  @RequestMapping(value="booklist", method=RequestMethod.GET)
+  public String booklist(Model model) {
+    model.addAttribute("books", bookRepository.findAll());
+    return "booklist";
+  }
+
+
   @RequestMapping(value="index", method=RequestMethod.GET)
   public String getIndex() {
-      return "index";
+    return "index";
   }
+
 
   @RequestMapping(value="*", method=RequestMethod.GET)
   public String toIndex() {
-      return "redirect:/index";
+    return "redirect:/index";
   }
   
 }
